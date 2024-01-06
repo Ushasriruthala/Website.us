99033b1d13524872c2cddca21343704b2efa59c7
Ushasriruthala <155825535+Ushasriruthala@users.noreply.github.com>
2024-01-06 19:22:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+ddocument.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add your code to handle the form submission (e.g., sending an email or saving to a database)
+    alert('Form submitted successfully!');
+});
+
